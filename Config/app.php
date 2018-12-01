97944c22b959dea057d79a9d40d0e43e038c54db
PIusha <piushakalyana@gmail.com>
2018-12-01 09:58:37 +0530

    Clear repo

--- Config/app.php
+++
@@ -1,9 +0,0 @@
-<?php
-define('DB_HOST','localhost');
-define('DB_USER_NAME','root');
-define('DB_PASSWORD','rootpass');
-define('DB_NAME','pluto');
-
-define('MODEL_PATH','App/Model');
-define('CONTROLLER_PATH','App/Controller');
-define('VIEW_PATH','App/View');
